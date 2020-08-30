af4dddc8415ecab6b7c04e5ce3383d06e2fbeace
Hala Alkhellow <halakhellow1996@gmail.com>
2020-08-31 02:21:11 +0300

    Updates

---
+++ precache-manifest.39c83619b4a643dac77de27878f0357c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be1370caa8008f573bd768d5c07ac97",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "05d9383f23f5e0a168db",
+    "url": "/pokedex/static/css/main.3bb100c5.chunk.css"
+  },
+  {
+    "revision": "1203cbcd5b0983cb03c7",
+    "url": "/pokedex/static/js/2.c423f07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.c423f07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d9383f23f5e0a168db",
+    "url": "/pokedex/static/js/main.b7ba14af.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
